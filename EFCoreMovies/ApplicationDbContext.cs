7e00bb30c68c4d23b0b5ba2b8338af413c3c0780
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-16 01:51:01 +0530

    wip

--- EFCoreMovies/ApplicationDbContext.cs
+++ EFCoreMovies/ApplicationDbContext.cs
@@ -17,9 +17,13 @@ namespace EFCoreMovies
             modelBuilder.Entity<Genre>().Property(p => p.Name).HasMaxLength(150).IsRequired();
 
             modelBuilder.Entity<Actor>().Property(p => p.Name).HasMaxLength(150).IsRequired();
+
+            modelBuilder.Entity<Cinema>().Property(p => p.Name).HasMaxLength(150).IsRequired();
         }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<Cinema> Cinemas { get; set; }
         
     }
 
---
+++ EFCoreMovies/Entities/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Entities
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Precision(precision: 9, scale: 2)] // or can be done in the ApplicationDbCOntext using HasPrecision
+        public decimal Price { get; set; } // add-migration after this and notice the warning
+
+
+
+
+    }
+}
