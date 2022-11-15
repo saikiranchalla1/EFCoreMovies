59b9613cf9b565a82c5662624a2e5b6535625787
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-16 00:09:30 +0530

    wip

--- EFCoreMovies/ApplicationDbContext.cs
+++ EFCoreMovies/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCoreMovies.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreMovies
 {
@@ -7,6 +8,8 @@ namespace EFCoreMovies
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<Genre> Genres { get; set; }
     }
 
 }
