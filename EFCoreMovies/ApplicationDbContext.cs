e6cf2566dccf1a5d4b0ed92937e6008ec277754d
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-16 03:16:42 +0530

    wip

--- EFCoreMovies/ApplicationDbContext.cs
+++ EFCoreMovies/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EFCoreMovies.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace EFCoreMovies
 {
@@ -20,36 +21,7 @@ namespace EFCoreMovies
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            //  modelBuilder.Entity<Genre>(g => g.Identifier); <- Adding a primary key using overidden methods
-
-
-            modelBuilder.Entity<Genre>().Property(p => p.Name).HasMaxLength(150).IsRequired();
-
-            modelBuilder.Entity<Actor>().Property(p => p.Name).HasMaxLength(150).IsRequired();
-
-            modelBuilder.Entity<Cinema>().Property(p => p.Name).HasMaxLength(150).IsRequired();
-
-            modelBuilder.HasPostgresExtension("postgis"); // ensure that postgis extension is installed on the server
-
-            modelBuilder.Entity<Movie>().Property(p => p.Title).HasMaxLength(150).IsRequired();
-
-            modelBuilder.Entity<Movie>().Property(p => p.PosterURL).HasMaxLength(150).IsUnicode(false); // IsUnicode allows us to define the characters that we want to accept.
-            // Add migration afater the above step and checkout the type of PosterURL. It should be varchar
-
-
-            modelBuilder.Entity<CinemaOffer>().Property(p => p.DiscountPercentage).HasPrecision(5, 2);
-            modelBuilder.Entity<CinemaOffer>().Property(p => p.Begin).HasColumnType("date");
-            modelBuilder.Entity<CinemaOffer>().Property(p => p.End).HasColumnType("date");
-
-            modelBuilder.Entity<CinemaHall>().Property(p => p.Cost).HasPrecision(5, 2);
-
-            modelBuilder.Entity<CinemaHall>().Property(p => p.CinemaHallType).HasDefaultValue(CinemaHallType.TwoDimensions);
-
-            modelBuilder.Entity<MovieActor>().HasKey(p => new { p.MovieId, p.ActorId });
-
-            modelBuilder.Entity<MovieActor>().Property(p => p.Character).HasMaxLength(150);
-
-            modelBuilder.Entity<Actor>().Property(p => p.Biography).HasColumnType("nvarchar(max)");
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Actor> Actors { get; set; }
