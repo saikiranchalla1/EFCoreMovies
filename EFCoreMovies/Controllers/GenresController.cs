3df24b7801d293739c94d12451b03e60e7af0490
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-17 02:05:24 +0530

    wip

--- EFCoreMovies/Controllers/GenresController.cs
+++ EFCoreMovies/Controllers/GenresController.cs
@@ -44,5 +44,11 @@ namespace EFCoreMovies.Controllers
 
             return genre;
         }
+
+        [HttpGet("filter")]
+        public async Task<IEnumerable<Genre>> Filter(string name)
+        {
+            return await context.Genres.Where(g => g.Name.Contains(name)).ToListAsync();
+        }
     }
 }
