d49e247bd489ce6a005b903fbfa59e527959b32d
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-17 03:23:04 +0530

    wip

--- EFCoreMovies/Controllers/CinemasController.cs
+++ EFCoreMovies/Controllers/CinemasController.cs
@@ -4,6 +4,8 @@ using EFCoreMovies.DTOs;
 using EFCoreMovies.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+using NetTopologySuite.Geometries;
 
 namespace EFCoreMovies.Controllers
 {
@@ -26,6 +28,27 @@ namespace EFCoreMovies.Controllers
             // return await context.Cinemas.ToListAsync(); // THIS WILL RETURN AN ERROR BECAUSE WE CANNOT SERIALIZE THE Point type
 
             return await context.Cinemas.ProjectTo<CinemaDTO>(mapper.ConfigurationProvider).ToListAsync();
+        }
+
+        [HttpGet("closetome")]
+        public async Task<ActionResult> Get(double latitude = -69.940154, double longitude = 18.483280)
+        {
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+
+            var myLocation = geometryFactory.CreatePoint(new Coordinate(longitude, latitude));
+
+            var maxDistanceInMeters = 2000; // 2 kms
+
+            var cinemas = await context.Cinemas
+                .OrderBy(c => c.Location.Distance(myLocation))
+                .Where(c => c.Location.IsWithinDistance(myLocation, maxDistanceInMeters))
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    Distance = Math.Round(c.Location.Distance(myLocation))
+                }).ToListAsync();
+
+            return Ok(cinemas);
         }
     }
 }
