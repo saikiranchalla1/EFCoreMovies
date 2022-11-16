b4fa76ac522e255783b0661536a68e6b8da52184
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-17 03:28:50 +0530

    wip

---
+++ EFCoreMovies/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using EFCoreMovies.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public MoviesController(ApplicationDbContext context) { 
+            this.context = context
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Movie>> Get(int id)
+        {
+            var movie = await context.Movies
+                .Include(m => m.Genres)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movie is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+    }
+}
