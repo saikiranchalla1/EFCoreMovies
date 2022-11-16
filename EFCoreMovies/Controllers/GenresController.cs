397d11d7549084c55047d1741464e25a28fe737a
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-17 02:02:31 +0530

    wip

--- EFCoreMovies/Controllers/GenresController.cs
+++ EFCoreMovies/Controllers/GenresController.cs
@@ -23,5 +23,26 @@ namespace EFCoreMovies.Controllers
             // using AsNoTracking improves the performance of the application
             // use AsTracking to allow for tracking
         }
+
+        [HttpGet("first")]
+        // public async Task<Genre> GetFirst()
+        public async Task<ActionResult<Genre>> GetFirst()
+        {
+            // return await context.Genres.FirstAsync();
+
+            // return await context.Genres.FirstAsync(g => g.Name.Contains("m")); // <= return first genre that contains the letter "m"
+
+            // return await context.Genres.FirstAsync(g => g.Name.Contains("z")); // <= Throws an exception because there is no Genre with the letter 'z'
+            // return await context.Genres.FirstOrDefaultAsync(g => g.Name.Contains("z")); // <= Returns null
+
+            var genre = await context.Genres.FirstOrDefaultAsync(g => g.Name.Contains("z"));
+
+            if (genre is null)
+            {
+                return NotFound(); // for this to work change return type to Task<ActionResult<Genre>>
+            }
+
+            return genre;
+        }
     }
 }
