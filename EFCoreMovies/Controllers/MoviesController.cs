f7a4f26f6043f7f9a36f07d3ad858ba2765adb3a
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-17 19:31:48 +0530

    wip

--- EFCoreMovies/Controllers/MoviesController.cs
+++ EFCoreMovies/Controllers/MoviesController.cs
@@ -41,8 +41,8 @@ namespace EFCoreMovies.Controllers
         public async Task<ActionResult<MovieDTO>> GetForMovieDTO(int id)
         {
             var movie = await context.Movies
-                .Include(m => m.Genres.OrderByDescending(g => g.Name).Where(g => !g.Name.Contains("m")))
-                .Include(m => m.CinemaHalls.OrderByDescending(ch => ch.Cinema.Name))
+                .Include(m => m.Genres)
+                .Include(m => m.CinemaHalls)
                     .ThenInclude(ch => ch.Cinema)
                 .Include(m => m.MoviesActors)
                     .ThenInclude(ma => ma.Actor)
@@ -59,6 +59,30 @@ namespace EFCoreMovies.Controllers
 
             return movieDTO;
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<MovieDTO>> GetForFilteringAndOrdering(int id)
+        {
+            var movie = await context.Movies
+                .Include(m => m.Genres.OrderByDescending(g => g.Name).Where(g => !g.Name.Contains("m")))
+                .Include(m => m.CinemaHalls.OrderByDescending(ch => ch.Cinema.Name))
+                    .ThenInclude(ch => ch.Cinema)
+                .Include(m => m.MoviesActors)
+                    .ThenInclude(ma => ma.Actor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie is null)
+            {
+                return NotFound();
+            }
+
+            var movieDTO = mapper.Map<MovieDTO>(movie);
+
+            movieDTO.Cinemas = movieDTO.Cinemas.DistinctBy(x => x.Id).ToList();
+
+            return movieDTO;
+        }
+
 
     }
 }
