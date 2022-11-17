7a03b2ac9c496d782f9c34e3589a422d7c1149fb
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-17 19:27:11 +0530

    wip

--- EFCoreMovies/Controllers/MoviesController.cs
+++ EFCoreMovies/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using EFCoreMovies.DTOs;
 using EFCoreMovies.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,17 +11,22 @@ namespace EFCoreMovies.Controllers
     public class MoviesController : ControllerBase
     {
         private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
 
-        public MoviesController(ApplicationDbContext context) { 
-            this.context = context
+        public MoviesController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
         }
 
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<Movie>> Get(int id)
+        public async Task<ActionResult<MovieDTO>> Get(int id)
         {
             var movie = await context.Movies
                 .Include(m => m.Genres)
+                .Include(m => m.CinemaHalls)
+                    .ThenInclude(ch => ch.Cinema)
                 .FirstOrDefaultAsync(x => x.Id == id);
 
             if (movie is null)
@@ -30,5 +36,29 @@ namespace EFCoreMovies.Controllers
 
             return Ok(movie);
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<MovieDTO>> GetForMovieDTO(int id)
+        {
+            var movie = await context.Movies
+                .Include(m => m.Genres.OrderByDescending(g => g.Name).Where(g => !g.Name.Contains("m")))
+                .Include(m => m.CinemaHalls.OrderByDescending(ch => ch.Cinema.Name))
+                    .ThenInclude(ch => ch.Cinema)
+                .Include(m => m.MoviesActors)
+                    .ThenInclude(ma => ma.Actor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie is null)
+            {
+                return NotFound();
+            }
+
+            var movieDTO = mapper.Map<MovieDTO>(movie);
+
+            movieDTO.Cinemas = movieDTO.Cinemas.DistinctBy(x => x.Id).ToList(); // <= removies duplicate cinemas
+
+            return movieDTO;
+        }
+
     }
 }
--- EFCoreMovies/Utilities/AutoMapperProfiles.cs
+++ EFCoreMovies/Utilities/AutoMapperProfiles.cs
@@ -12,6 +12,15 @@ namespace EFCoreMovies.Utilities
             CreateMap<Cinema, CinemaDTO>()
                 .ForMember(dto => dto.Latitude, ent => ent.MapFrom(p => p.Location.Y))
                 .ForMember(dto => dto.Longitude, ent => ent.MapFrom(p => p.Location.X));
+
+            CreateMap<Genre, GenreDTO>();
+
+            CreateMap<Movie, MovieDTO>()
+                .ForMember(dto => dto.Genres, ent => ent.MapFrom(p => p.Genres.OrderByDescending(g => g.Name)))
+                .ForMember(dto => dto.Cinemas, ent =>
+                    ent.MapFrom(p => p.CinemaHalls.OrderByDescending(ch => ch.Cinema.Name)
+                                                  .Select(c => c.Cinema)))
+                .ForMember(dto => dto.Actors, ent => ent.MapFrom(p => p.MoviesActors.Select(ma => ma.Actor)));
         }
     }
 }
